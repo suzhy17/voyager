cb68c0f71293a0154bf1261a721d1950b8a64dd9
suzhy <suzhy@naver.com>
2017-09-25 09:03:35 +0900

    캐시 샘플 코드 추가

--- voyager-core/src/main/java/com/suzhy/voyager/core/cache/repository/BookRepositoryImpl.java
+++ voyager-core/src/main/java/com/suzhy/voyager/core/cache/repository/BookRepositoryImpl.java
@@ -1,6 +1,6 @@
-package com.suzhy.voyager.core.repository;
+package com.suzhy.voyager.core.cache.repository;
 
-import com.suzhy.voyager.core.dto.Book;
+import com.suzhy.voyager.core.dto.BookDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
@@ -11,9 +11,9 @@ public class BookRepositoryImpl implements BookRepository {
 
 	@Override
 	@Cacheable("booksCache")
-	public Book getByIsbn(String isbn) {
+	public BookDto getByIsbn(String isbn) {
 		log.info(" non-cache run");
-		return new Book(isbn, "Some book");
+		return new BookDto(isbn, "Some book");
 	}
 
 	// Don't do this at home
